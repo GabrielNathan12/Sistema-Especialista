4fffa863a11be36123c27a5e32c68a9c1e319706
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-06-02 12:05:31 -0300

    fet: exercicio pronto

--- Prolog.pl
+++ Prolog.pl
@@ -1,20 +1,23 @@
-
-pway(a, b, 10).
-pway(b, c, 15).
-pway(d, c, 5).
-pway(d, b, 10).
-
-% Predicado caso eles ja estejam conectados vice-versa
-
+% Predicado caso eles já estejam conectados e vice-versa
 connected(X, Y, N) :- pway(X, Y, N).
-connected(X , Y, N) :- pway(Y, X, N).
+connected(X, Y, N) :- pway(Y, X, N).
 
-solve(Y, X ,P, N) :- solve2(X, Y, [X], 0, P, N).
-% Encontra uma passagem em X para A com determinada distancia
-solve2(X, Y, V, Tamanho, P, N) :- connected(X, A, Distancia).
-% O menber faz a pesquisa evitando se o A ja foi visitado
-+menber(A, V).
-Tamanho :- Total is Tamanho + Distancia.
+solve(Y, X, P, N) :- solve2(X, Y, [X], 0, P, N).
+solve2(X, X, Visitado, Tamanho, Visitado, Tamanho). % Caso base
+solve2(X, Y, Visitado, Tamanho, P, N) :-
+    connected(X, A, Distancia),                     % Encontra uma passagem de X para A com determinada Distancia
+    \+ member(A, Visitado),                         % O member faz a pesquisa evitando se o A já foi visitado
+    TamanhoTotal is Tamanho + Distancia,            % Aumenta o comprimento acumulado
+    solve2(A, Y, [A|Visitado], TamanhoTotal, P, N). % Chame recursivamente solve com A fazendo a prox busca
 
-solve2(A, Y, [A|Visitado], TamanhoTotal, P, N).
-% Chame recursivamente solve com A fazendo a prox busca
+% Encontrar todos os caminhos ordenados por comprimento
+solve_all(X, Y, Paths) :-
+    bagof([P, N], solve(X, Y, P, N), AllPaths),
+    sort_paths(AllPaths, Paths).
+  
+  sort_paths(Paths, SortedPaths) :-
+    map_list_to_pairs(length_of_path, Paths, Pairs),
+    keysort(Pairs, SortedPairs),
+    pairs_values(SortedPairs, SortedPaths).
+  
+  length_of_path([_, N], N).
