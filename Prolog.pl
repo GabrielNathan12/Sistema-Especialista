0263319f9436fe613c29004ae2dc7e5155c3a686
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-06-02 15:32:37 -0300

    feat: Exercicio pronto

--- Prolog.pl
+++ Prolog.pl
@@ -10,14 +10,16 @@ solve2(X, Y, Visitado, Tamanho, P, N) :-
     TamanhoTotal is Tamanho + Distancia,            % Aumenta o comprimento acumulado
     solve2(A, Y, [A|Visitado], TamanhoTotal, P, N). % Chame recursivamente solve com A fazendo a prox busca
 
-% Encontrar todos os caminhos ordenados por comprimento
-solve_all(X, Y, Paths) :-
-    bagof([P, N], solve(X, Y, P, N), AllPaths),
-    sort_paths(AllPaths, Paths).
+% Predicado para ordenar os caminhos pelo menor ao maior
+solve_order(X, Y, Caminho) :-
+    bagof([P, N], solve(X, Y, P, N ), TodosCaminhos),        % Predicado que irá pegar todas as soluções possíveis
+    paths(TodosCaminhos, Caminho).                          % Predico auxiliar que será usado para pegar todas soluções possíveis, 
+                                                            % ordená-las e colocar na variável TodosCaminhos
   
-  sort_paths(Paths, SortedPaths) :-
-    map_list_to_pairs(length_of_path, Paths, Pairs),
-    keysort(Pairs, SortedPairs),
-    pairs_values(SortedPairs, SortedPaths).
-  
-  length_of_path([_, N], N).
+paths(Caminho, CaminhosOrd) :-
+    map_list_to_pairs(length_path, Caminho, Pares),           % Predicado que irá mapear a lista de pares (chaves-valor) e guardalas em Pares
+    sort(Pares, ParesOrd),                                    % Predicado que irá ordenar os pares do menor elemento ao maior elemento
+    pairs_values(ParesOrd, CaminhosOrd).                      % Predicado que irá extrair os resultados ordenados
+
+length_path([_, N], N).
+                                                              % Predicado que irá imprimir na tela os valores
